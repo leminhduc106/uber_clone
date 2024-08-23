dc73457291463f017e14b5e673c2701399dd1b57
Denis Le <denis.l@itcgroup.io>
2024-08-23 22:54:40 +0700

    init commit

---
+++ uber_app/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { StatusBar, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500 font-bold">Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
