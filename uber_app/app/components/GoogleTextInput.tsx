7a8440d565024638a7ce2e24d0979a6763fa95e3
Denis Le <denis.l@itcgroup.io>
2024-09-07 21:42:07 +0700

    feat: Implement GoogleTextInput component

--- uber_app/app/components/GoogleTextInput.tsx
+++ uber_app/app/components/GoogleTextInput.tsx
@@ -1,5 +1,7 @@
+import { icons } from "@/constants";
 import { GoogleInputProps } from "@/types/type";
-import { View, Text } from "react-native";
+import { View, Image } from "react-native";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 const GoogleTextInput = ({
     icon,
@@ -10,7 +12,58 @@ const GoogleTextInput = ({
 }: GoogleInputProps) => {
     return (
         <View className={`flex flex-row items-center justify-center relative z-50 rounded-xl mb-5 ${containerStyle}`}>
-            <Text>Search</Text>
+            <GooglePlacesAutocomplete
+                placeholder={"Search location"}
+                fetchDetails={true}
+                debounce={300}
+                styles={{
+                    textInputContainer: {
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        borderRadius: 20,
+                        marginHorizontal: 20,
+                        position: 'relative',
+                        shadowColor: '#d4d4d4'
+                    },
+                    textInput: {
+                        backgroundColor: textInputBackgroundColor || 'white',
+                        fontSize: 16,
+                        fontWeight: 600,
+                        marginTop: 10,
+                        width: '100%',
+                        borderRadius: 20,
+                    },
+                    listView: {
+                        backgroundColor: textInputBackgroundColor || 'white',
+                        position: 'relative',
+                        top: 0,
+                        width: '100%',
+                        borderRadius: 10,
+                        shadowColor: '#d4d4d4',
+                        zIndex: 1000,
+                    }
+                }}
+                onPress={(data, details = null) => {
+                    handlePress({
+                        latitude: details?.geometry.location.lat!,
+                        longitude: details?.geometry.location.lng!,
+                        address: data.description,
+                    });
+                }}
+                query={{
+                    key: process.env.EXPO_PUBLIC_GOOLGE_API_KEY,
+                    language: 'en',
+                }}
+                renderLeftButton={() => (
+                    <View className="items-center justify-center">
+                        <Image source={icon || icons.search} className="w-5 h-5" resizeMode="contain" />
+                    </View>
+                )}
+                textInputProps={{
+                    placeholderTextColor: 'gray',
+                    placeholder: initialLocation ?? "Search location",
+                }}
+            />
         </View>
     );
 }
