a147c25b12304930d5a4d5f22f24d4851f38a959
Denis Le <denis.l@itcgroup.io>
2024-11-24 10:50:06 +0700

    Implement Stripe integration for payment ride

---
+++ uber_app/app/(api)/(stripe)/create+api.ts
@@ -0,0 +1,49 @@
+// Backend function to create a payment intent
+
+import { Stripe } from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+export async function POST(request: Request) {
+    const body = await request.json();
+    const { name, email, amount } = body;
+
+    if (!name || !email || !amount) {
+        return new Response(JSON.stringify({ error: 'Missing required fields' }), { status: 400 });
+    }
+
+    let customer;
+
+    const existingCustomer = await stripe.customers.list({ email: email });
+
+    if (existingCustomer.data.length > 0) {
+        customer = existingCustomer.data[0];
+    } else {
+       const newCustomer = await stripe.customers.create({ email: email, name: name });
+       customer = newCustomer;
+    }
+
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+        {customer: customer.id},
+        {apiVersion: '2024-11-20.acacia'}
+      );
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: parseInt(amount) * 100,
+        currency: 'usd',
+        customer: customer.id,
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter
+        // is optional because Stripe enables its functionality by default.
+        automatic_payment_methods: {
+          enabled: true,
+          allow_redirects: 'never',
+        },
+      });
+    
+      return new Response(
+        JSON.stringify({
+            paymentIntent: paymentIntent,
+            ephemeralKey: ephemeralKey,
+            customer: customer.id,
+      })
+    );
+}
